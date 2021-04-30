d1056da1f7f43e821107666a5feba959b9784b2b
gaku yamamoto <githubgemini1124@gmail.com>
2021-04-30 12:09:41 +0900

    implement geofencing

--- iBeaconApp/ViewController.swift
+++ iBeaconApp/ViewController.swift
@@ -24,8 +24,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UNUserNotific
         //とりあえずdelegateを呼び出す為に記述
         locationManager.delegate = self
         //この記述は必要？
-        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
-        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = 1
         //バックグラウンドでのロケーション更新を許可する
         locationManager.allowsBackgroundLocationUpdates = true
         //ロケーション更新の自動中断をオフに設定する
@@ -46,15 +46,28 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UNUserNotific
             let uuid: NSUUID! = NSUUID(uuidString: UUIDList[i].lowercased())
             let identifierStr: String = "Beacon:No.\(i)"
             let beaconRegion = CLBeaconRegion(uuid: uuid as UUID, identifier: identifierStr)
-            beaconRegion.notifyEntryStateOnDisplay = true
+            //beaconRegion.notifyEntryStateOnDisplay = true
             beaconRegion.notifyOnEntry = true
             beaconRegion.notifyOnExit = true
             locationManager.startMonitoring(for: beaconRegion)
             locationManager.startRangingBeacons(satisfying: beaconRegion.beaconIdentityConstraint)
         }
         
+        startGeofenceMonitering()
+        
         confirmLocalNotification()
     }
+    
+    private func startGeofenceMonitering() {
+            let monitoringCordinate = CLLocationCoordinate2DMake(35.62221687, 139.70470641) ///自分の家
+            let monitoringRegion = CLCircularRegion.init(center: monitoringCordinate, radius: 300, identifier: "MyHome")
+            monitoringRegion.notifyOnExit = true
+            monitoringRegion.notifyOnEntry = true
+            //locationManager.maximumRegionMonitoringDistance(
+            locationManager.startMonitoring(for: monitoringRegion)
+            locationManager.requestState(for: monitoringRegion)
+            locationManager.startUpdatingLocation()
+        }
     
     func confirmLocalNotification() {
         //許可をもらう通知タイプの種類を定義
@@ -87,9 +100,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UNUserNotific
         manager.requestState(for: region)
     }
     
-    func locationManager(_ manager: CLLocationManager, didDetermineState state: CLRegionState, for region: CLRegion) {
-    }
-    
     func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error) {
         print("monitoringDidFailFor was fired")
     }
@@ -100,21 +110,42 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UNUserNotific
     
     func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
         print("didEnterregion was fired !!!")
-        scheduleNotification(
-            title: "didEnterregion was fired !!!",
-            body: "対象のリージョンに入りました。"
-        )
+        if region is CLCircularRegion {
+            scheduleNotification(title: "CLCircularRegion", body: "CLCircularRegion didEnterRegion")
+        } else {
+            scheduleNotification(
+                title: "didEnterregion was fired !!!",
+                body: "対象のリージョンに入りました。"
+            )
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
         print("didExitRegion was fired !!!")
-        scheduleNotification(
-            title: "didExitRegion was fired !!!",
-            body: "対象のリージョンから離れました。"
-        )
+        if region is CLCircularRegion {
+            scheduleNotification(
+                title: "CLCircularRegion",
+                body: "CLCircularRegion didExitRegion"
+            )
+        } else {
+            scheduleNotification(
+                title: "didExitRegion was fired !!!",
+                body: "対象のリージョンから離れました。"
+            )
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didDetermineState state: CLRegionState, for region: CLRegion) {
+            if state == .inside {
+                print("you are inside")
+            } else if state == .outside {
+                print("you are outside")
+            } else {
+                print("unkown")
+            }
     }
     
     
@@ -125,7 +156,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UNUserNotific
         print("locations",locations)
         if let location = locations.last {
                 print("New location is \(location)")
-            scheduleNotification(title: "New location", body: location.debugDescription)
+            //scheduleNotification(title: "New location", body: location.debugDescription)
         }
     }
     
